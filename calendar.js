28d86dd16bb36fcbf806d9c38e5411614ddefa75
Badawy-tv <badawytv76@gmail.com>
2026-03-01 22:18:08 +0300

    Switch to local automatic Hijri calendar

--- calendar.js
+++ calendar.js
@@ -1,38 +1,38 @@
-const now = new Date();
-const month = now.getMonth() + 1;
-const year = now.getFullYear();
+document.addEventListener("DOMContentLoaded", function() {
 
-fetch("https://api.aladhan.com/v1/calendarByCity?city=Nairobi&country=Kenya&method=2&month=" + month + "&year=" + year)
-.then(function(res){ return res.json(); })
-.then(function(data){
+const today = new Date();
+const hijriToday = new Intl.DateTimeFormat('en-TN-u-ca-islamic', {
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric'
+}).format(today);
 
-let days = data.data;
-let today = now.getDate();
+const monthFormatter = new Intl.DateTimeFormat('en-TN-u-ca-islamic', {
+  month: 'long',
+  year: 'numeric'
+});
 
-let table = "<table style='width:100%;border-collapse:collapse;text-align:center;'>";
+const dayFormatter = new Intl.DateTimeFormat('en-TN-u-ca-islamic', {
+  day: 'numeric'
+});
 
-table += "<tr style='background:#111;color:gold;'><th>Hijri</th><th>Fajr</th><th>Dhuhr</th><th>Asr</th><th>Maghrib</th><th>Isha</th></tr>";
+let table = "<h3 style='color:gold;margin-bottom:15px;'>" + monthFormatter.format(today) + "</h3>";
+table += "<table style='width:100%;border-collapse:collapse;text-align:center;background:#111;color:white;'>";
+table += "<tr style='background:#000;color:gold;'><th>Day</th></tr>";
 
-for(let i=0;i<days.length;i++){
+for(let i=1;i<=30;i++){
+  let testDate = new Date(today.getFullYear(), today.getMonth(), i);
+  let hijriDay = dayFormatter.format(testDate);
 
-let day = days[i];
-let isToday = (parseInt(day.date.gregorian.day) === today);
+  let isToday = (testDate.getDate() === today.getDate());
 
-table += "<tr style='background:" + (isToday ? "#0a7c3a" : "#1a1a1a") + ";'>";
-table += "<td>" + day.date.hijri.day + " " + day.date.hijri.month.en + "</td>";
-table += "<td>" + day.timings.Fajr.split(" ")[0] + "</td>";
-table += "<td>" + day.timings.Dhuhr.split(" ")[0] + "</td>";
-table += "<td>" + day.timings.Asr.split(" ")[0] + "</td>";
-table += "<td>" + day.timings.Maghrib.split(" ")[0] + "</td>";
-table += "<td>" + day.timings.Isha.split(" ")[0] + "</td>";
-table += "</tr>";
+  table += "<tr style='background:" + (isToday ? "#0a7c3a" : "#1a1a1a") + ";'>";
+  table += "<td>" + hijriDay + "</td>";
+  table += "</tr>";
 }
 
 table += "</table>";
 
 document.getElementById("hijriCalendar").innerHTML = table;
 
-})
-.catch(function(){
-document.getElementById("hijriCalendar").innerHTML = "Unable to load Islamic calendar.";
 });
