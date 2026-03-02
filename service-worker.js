1672653ca96d92ff3a1646e02da6d90b505a6cfe
Badawy-tv <badawytv76@gmail.com>
2026-03-02 17:37:20 +0300

    Full professional upgrade: Dark mode, Dropdown, Quiz, Courses, SW fix

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+self.addEventListener("activate",event=>{event.waitUntil(self.clients.claim());});
+self.addEventListener("fetch",event=>{event.respondWith(fetch(event.request));});
 self.addEventListener("install", event => {
   console.log("Service Worker installed");
 });
