e1e54ad6c7288a3f32306039a92914545d5d0d51
Badawy-tv <badawytv76@gmail.com>
2026-03-08 23:20:09 +0300

    Remove hamburger menu completely

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
-const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
-if(hamburger){
-hamburger.onclick = function(){
 navMenu.classList.toggle("active");
 }
 }
@@ -11,11 +8,8 @@ navMenu.classList.toggle("active");
 
 document.addEventListener("DOMContentLoaded", function(){
 
-const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
-if(hamburger){
-hamburger.addEventListener("click", function(){
 navMenu.classList.toggle("active");
 });
 }
