02981559e08f9a7e90f8c172a46ec95a17d0830c
Badawy-tv <badawytv76@gmail.com>
2026-03-08 21:52:50 +0300

    Fix navbar and working hamburger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,17 @@ navMenu.classList.toggle("active");
 }
 }
 });
+
+document.addEventListener("DOMContentLoaded", function(){
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+if(hamburger){
+hamburger.addEventListener("click", function(){
+navMenu.classList.toggle("active");
+});
+}
+
+});
+
