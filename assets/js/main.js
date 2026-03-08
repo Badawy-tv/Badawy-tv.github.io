0a42accfb038d7a708e3308530d5787a0f4f07ac
Badawy-tv <badawytv76@gmail.com>
2026-03-08 22:15:28 +0300

    Fix search panel opening automatically

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,24 @@ navMenu.classList.toggle("active");
 
 });
 
+
+document.addEventListener("DOMContentLoaded", function(){
+
+const openSearch = document.getElementById("floating-search");
+const panel = document.getElementById("search-panel");
+const close = document.getElementById("close-search");
+
+if(openSearch){
+openSearch.addEventListener("click", function(){
+panel.style.display = "block";
+});
+}
+
+if(close){
+close.addEventListener("click", function(){
+panel.style.display = "none";
+});
+}
+
+});
+
