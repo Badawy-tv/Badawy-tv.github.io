667998dcb21cffaabce046a00ad9e823a62b1d94
Badawy-tv <badawytv76@gmail.com>
2026-03-08 23:25:07 +0300

    Remove navigation system completely

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
-const navMenu = document.getElementById("nav-menu");
 
-navMenu.classList.toggle("active");
 }
 }
 });
 
 document.addEventListener("DOMContentLoaded", function(){
 
-const navMenu = document.getElementById("nav-menu");
 
-navMenu.classList.toggle("active");
 });
 }
 
