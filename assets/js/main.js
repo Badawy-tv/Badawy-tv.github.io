d4aceddfd4f46f2e108b86630131c76b41eb252b
Badawy-tv <badawytv76@gmail.com>
2026-03-08 15:46:58 +0300

    Fix hamburger menu visibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
-hamburger.addEventListener("click", function(){
+if(hamburger){
+hamburger.onclick = function(){
 navMenu.classList.toggle("active");
+}
+}
 });
